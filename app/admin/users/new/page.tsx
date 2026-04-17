349a617e90a876da541603246259cf59230c1273
Steve Bennett <66376+stebennett@users.noreply.github.com>
2026-04-17 17:53:39 +0100

    feat(admin): add new-user page

---
+++ app/admin/users/new/page.tsx
@@ -0,0 +1,9 @@
+import NewUserForm from "./NewUserForm"
+export default function NewUserPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">New user</h1>
+      <NewUserForm />
+    </div>
+  )
+}
